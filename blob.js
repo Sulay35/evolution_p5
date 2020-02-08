365d30b0bd775e223d936ad1b444464d2e8e05a8
Sulay35 <sulaymane.dagnet@laposte.net>
2020-02-08 11:44:05 +0100

    new changes

--- blob.js
+++ blob.js
@@ -1,21 +1,33 @@
 class Blob{
-    constructor(x1,y1){
+    constructor(x1,y1, fam){
         this.pos= createVector(x1,y1);
 
         this.velocity = createVector(2,1);
+
         this.xoff = random(0,width*2);
         this.yoff = random(0,height*2);
+
+       
+
+        this.fam = fam;
     }
     draw(){
-        circle(this.pos.x, this.pos.y, 10); 
+        push()
+        stroke(255,100);
+        if(this.fam == "red") fill(255,0,0,220);
+        else if(this.fam == "green") fill(0,255,0,220);
+        else if(this.fam == "blue") fill(0,0,255,220);
+        circle(this.pos.x, this.pos.y, 15); 
+        pop()
     }
     update(){
 
-        this.pos.x = map(noise(this.xoff),0,1,0,width);
-        this.pos.y = map(noise(this.yoff),0,1,0,height);
+        this.pos.x = map(noise(this.xoff),0,1,0-50,width+50);
+        this.pos.y = map(noise(this.yoff),0,1,0-50,height+50);
 
-        this.xoff += 0.005;
-        this.yoff += 0.005;
+        if(this.fam == "red") {this.xoff += 0.005; this.yoff += 0.005}
+        else if(this.fam == "green") {this.xoff += 0.003; this.yoff += 0.003}
+        else if(this.fam == "blue"){ this.xoff += 0.001; this.yoff += 0.001}
 
         
         this.draw();
--- sketch.js
+++ sketch.js
@@ -1,12 +1,22 @@
 let blobs = [];
+
+let reds = [];
+let blues = [];
+let greens = [];
+
+
 function setup() {
     createCanvas(400,400);
     frameRate(60)
     for(var i = 0; i < 100; i++){
         let x = random(0, width);
         let y = random(0, height);
-        blobs.push(new Blob(x, y));
-    
+        let fam = random(["red", "green", "blue"]);
+        if(fam == "red") reds.push(new Blob(x, y, fam));
+        if(fam == "blue") blues.push(new Blob(x, y, fam));
+        if(fam == "green") greens.push(new Blob(x, y, fam));
+        blobs.push(new Blob(x, y, fam));
+        
     }
 }
 
@@ -15,6 +25,7 @@ function draw() {
     let fps = floor(frameRate());
     text(fps,10, 15)
     for(var i = 0; i < blobs.length; i++){
-        blobs[i].update()
+        blobs[i].update();
     }
+    
 }
