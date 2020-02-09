c5f68cb8be50e4521a9d76535bd253c7de0c63b8
Sulay35 <sulaymane.dagnet@laposte.net>
2020-02-09 08:35:25 +0100

    stable version

--- sketch.js
+++ sketch.js
@@ -23,51 +23,127 @@ let greens = [];
 
 // variables d'évolution :
 
-let popI = 100;
-let Brate = 0.1;
-let Drate = 0.511;
-let Rrate = 0.0;
+let popI = 10;
+
+let red = {
+    initPop:0,  // initial population
+    Brate: 0.07, // spontaneous birth rate
+    Drate: 0.5, // spontaneous death rate
+    Rrate: 0.1, // spontaneous replication rate
+}
+
+let blue = {
+    initPop:0,  // initial population
+    Brate: 0.1, // spontaneous birth rate
+    Drate: 0.51, // spontaneous death rate
+    Rrate: 0.1, // spontaneous replication rate
+    toGreen:0.001, // spontaneous green transformation rate
+    toRed:0.2,
+}
+
+let green = {
+    initPop:0,  // initial population
+    Brate: 0.1, // spontaneous birth rate
+    Drate: 0.81, // spontaneous death rate
+    Rrate: 0.00, // spontaneous replication rate
+}
 
 function newGen(){
+    //----------------
+    // Blues :
     // kill
-    for(var i = 0; i < blues.length*Drate; i++){
+    for(var i = 0; i < blues.length*blue.Drate; i++){
         blues[i].dying = true;
     }
     // Born
-    for(var b = 0; b < blues.length*Brate; b++){
+    for(var b = 0; b < blues.length*blue.Brate; b++){
         let x = random(width);
         let y= random(height);
         let fam = "blue";
         let blob = new Blob(x, y, fam);
         blues.push(blob);
-        
-        //blues.push(blob);
-
     }
-     
     // Replicate :
-    for(var b = 0; b < blues.length*Rrate; b++){
+    for(var b = 0; b < blues.length*blue.Rrate; b++){
         let x = blues[i].x+random(2);
         let y = blues[i].y+random(2);
         let fam = "blue";
         let blob = new Blob(x, y, fam);
         blues.push(blob);
-        
-        //blues.push(blob);
+    }
+    // turn into green blob
+    for(var b = 0; b < blues.length*blue.toGreen; b++){
+        let x = blues[i].x+random(2);
+        let y = blues[i].y+random(2);
+        let fam = "green";
+        let blob = new Blob(x, y, fam);
+        greens.push(blob);
+    }
 
+    for(var b = 0; b < blues.length*blue.toRed; b++){
+        let x = blues[i].x + random(2);
+        let y = blues[i].x + random(2);
+        let fam = "red";
+        let blob = new Blob(x, y, fam);
+        reds.push(blob)
+    }
+
+    //---------------------------------------------------
+    // Greens :
+    // kill
+    for(var i = 0; i < greens.length*green.Drate; i++){
+        greens[i].dying = true;
+    }
+    // Born
+    for(var b = 0; b < greens.length*green.Brate; b++){
+        let x = random(width);
+        let y= random(height);
+        let fam = "green";
+        let blob = new Blob(x, y, fam);
+        greens.push(blob);
+    }
+     
+    // Replicate :
+    for(var b = 0; b < greens.length*green.Rrate; b++){
+        let x = greens[i].x+random(2);
+        let y = greens[i].y+random(2);
+        let fam = "green";
+        let blob = new Blob(x, y, fam);
+        greens.push(blob);
+    }
+
+    //---------------------------------------------
+    // Reds :
+    // Kill
+    for(var i = 0; i < reds.length*red.Drate; i++){
+        reds[i].dying = true;
+    }
+    // Born
+    for(var b = 0; b < reds.length*red.Brate; b++){
+        let x = random(width);
+        let y= random(height);
+        let fam = "red";
+        let blob = new Blob(x, y, fam);
+        reds.push(blob);
+    }
+     
+    // Replicate :
+    for(var b = 0; b < reds.length*red.Rrate; b++){
+        let x = reds[i].x+random(2);
+        let y = reds[i].y+random(2);
+        let fam = "red";
+        let blob = new Blob(x, y, fam);
+        reds.push(blob);
     }
 }
 
 
-
+// Population initialisation
 function initialize(population){
     for(let i = 0; i < population; i++){
         let x = random(0, width);
         let y = random(0, height); 
         let fam = "blue";
-
-        //if(fam == "blue") blobs.push(new Blob(x, y, fam));
-        
         blues.push(new Blob(x,y,fam))
     }
 
@@ -87,18 +163,37 @@ function setup() {
 
 let population;
 function draw() {
+    // Settings 
     background(80);
     let fps = floor(frameRate());
     text(fps,10, 15);
 
-    
+
     // update each blob
+    // blues
     for(var i = 0; i < blues.length; i++){
         blues[i].update();
         if(blues[i].dead == true){
             blues.splice(i,1)
         }
     }
+    // greens
+    for(var i = 0; i < greens.length; i++){
+        greens[i].update();
+        if(greens[i].dead == true){
+            greens.splice(i,1)
+        }
+    }
+
+    // reds 
+    for(var i; i < reds.length; i++){
+        reds[i].update();
+        if(reds[i].dead == true){
+            reds.splice(i, 1)
+        }
+    }
+
+
     population = blues.length;
     console.log(population);
 }
